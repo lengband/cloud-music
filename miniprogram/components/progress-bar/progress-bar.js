cf74b29fcaeeccfecfa22cadf2419d5372fd7f54
lengband <lengband@163.com>
2021-01-16 18:21:48 +0800

    歌曲播放的时候支持滑块

---
+++ miniprogram/components/progress-bar/progress-bar.js
@@ -0,0 +1,136 @@
+// components/progress-bar/progress-bar.js
+let movableAreaWidth = 0
+let movableViewWidth = 0
+const backgroundAudioManager = wx.getBackgroundAudioManager()
+let currentSec = -1 // 当前的秒数
+let duration = 0 // 当前歌曲的总时长
+let isMoving = false // 当前进度条是否在拖拽，解决：当进度条拖动的时候和updatetime时间有冲突的问题
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    showTime: {
+      currentTime: '00:00',
+      totalTime: '00:00'
+    },
+    movableDis: 0,
+    progress: 0
+  },
+
+  lifetimes: {
+    ready () {
+      this._getMovableDis()
+      this._bindBGMEvent()
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onChange (e) {
+      if (e.detail.source === 'touch') {
+        this.data.progress = e.detail.x / (movableAreaWidth - movableViewWidth) * 100
+        this.data.movableDis = e.detail.x
+        isMoving = true
+      }
+    },
+    onTouchEnd () {
+      const currentTimeFmt = this._dateFormat(Math.floor(backgroundAudioManager.currentTime))
+      this.setData({
+        progress: this.data.progress,
+        movableDis: this.data.movableDis,
+        ['showTime.currentTime']: `${currentTimeFmt.min}:${currentTimeFmt.sec}`,
+      })
+      backgroundAudioManager.seek(duration * this.data.progress / 100)
+      isMoving = false
+    },
+    _getMovableDis () {
+      const query = this.createSelectorQuery()
+      query.select('.movable-area').boundingClientRect()
+      query.select('.movable-view').boundingClientRect()
+      query.exec((rect) => {
+        movableAreaWidth = rect[0].width
+        movableViewWidth = rect[1].width
+      })
+    },
+    _bindBGMEvent () {
+      backgroundAudioManager.onPlay(() => {
+        console.log('onPlay');
+        isMoving = false
+      })
+      backgroundAudioManager.onStop(() => {
+        console.log('onPlay');
+      })
+      backgroundAudioManager.onPause(() => {
+        console.log('onPlay');
+      })
+      backgroundAudioManager.onWaiting(() => { // 拖拽到还未加载的歌曲部分
+        console.log('onPlay');
+      })
+      backgroundAudioManager.onCanplay(() => {
+        console.log('onCanplay');
+        if (backgroundAudioManager.duration === undefined) {
+          setTimeout(() => {
+            this._setTime()
+          }, 1000)
+        } else {
+          this._setTime()
+        }
+      })
+      backgroundAudioManager.onTimeUpdate(() => {
+        console.log('onTimeUpdate');
+        if (!isMoving) {
+          const currentTime = backgroundAudioManager.currentTime
+          const duration = backgroundAudioManager.duration
+          const sec = currentTime.toString().split('.')[0]
+          if (sec !== currentSec) {
+            const currentTimeFmt = this._dateFormat(currentTime)
+            const progress = currentTime / duration * 100
+            this.setData({
+              movableDis: (movableAreaWidth - movableViewWidth) * (currentTime / duration),
+              progress,
+              'showTime.currentTime': `${currentTimeFmt.min}:${currentTimeFmt.sec}`
+            })
+            currentSec = sec
+          }
+        }
+      })
+      backgroundAudioManager.onEnded(() => {
+        this.triggerEvent('musicEnd')
+      })
+      backgroundAudioManager.onError((res) => {
+        wx.showToast({
+          title: `错误：${res.errCode}`,
+        })
+      })
+    },
+    _setTime () {
+      duration = backgroundAudioManager.duration
+      const durationFmt = this._dateFormat(duration)
+      this.setData({
+        'showTime.totalTime': `${durationFmt.min}:${durationFmt.sec}`
+      })
+    },
+    _dateFormat (sec) {
+      const min = Math.floor(sec / 60)
+      sec = Math.floor(sec % 60)
+      return {
+        min: this._parse0(min),
+        sec: this._parse0(sec)
+      }
+    },
+    _parse0 (sec) { // 补0
+      return sec < 10 ? '0' + sec : sec
+    }
+  }
+})
