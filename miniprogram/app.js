8b348599bec9d26ba2dc0b87e42e2979bac4592e
lengband <lengband@163.com>
2021-01-20 11:56:38 +0800

    添加检测版本更新代码

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
+    this.checkUpdate()
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -14,7 +15,6 @@ App({
       })
       this.getOpenid()
     }
-
     this.globalData = {
       playingMusicId: -1,
       openid: -1
@@ -36,6 +36,25 @@ App({
         wx.setStorage({
           data: [], // 历史播放记录
           key: openid,
+        })
+      }
+    })
+  },
+  checkUpdate () {
+    const updateManager = wx.getUpdateManager()
+    // 检测版本更新
+    updateManager.onCheckForUpdate(res => {
+      if (res.hasUpdate) {
+        updateManager.onUpdateReady(() => {
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用',
+            success: res => {
+              if (res.confirm) {
+                updateManager.applyUpdate()
+              }
+            }
+          })
         })
       }
     })
